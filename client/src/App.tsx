54938eac0bd1d0edc435be021ac81abe9911aa47
Valentina Osorio <va.os.consulting@gmail.com>
2025-02-06 21:47:54 +0000

    trying to deploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 // import { useEffect, useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { ProductsPage } from './Pages/ProductsPage';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ProductDetails } from './Pages/ProductDetails';
 import { Header } from './components/Header';
-import { ShoppingCartProvider } from './components/ShoppingCartContext';
+// import { ShoppingCartProvider } from './components/ShoppingCartContext';
 import { UserProvider } from './components/UserContext';
 import { SignIn } from './Pages/Sign-In';
 import { SignUpForm } from './Pages/Sign-Up';
@@ -14,17 +14,14 @@ export default function App() {
   return (
     <>
       <UserProvider>
-        <ShoppingCartProvider>
-          <Header />
-
-          <Routes>
-            <Route path="/" element={<ProductsPage />} />
-            <Route path="/product/:productId" element={<ProductDetails />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/sign-up" element={<SignUpForm />} />
-          </Routes>
-        </ShoppingCartProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductsPage />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Routes>
       </UserProvider>
     </>
   );
